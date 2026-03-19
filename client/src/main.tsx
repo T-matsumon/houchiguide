c700e289eaacbadf80ecce54f17ff8efb0b15c04
T-matsumon <takgak634@gmail.com>
2026-03-19 15:19:34 +0900

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // 1. これを追加
 import App from "./App";
 import "./index.css";
 
-// 2. <App /> を <HashRouter> で囲む
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <App />
 );
